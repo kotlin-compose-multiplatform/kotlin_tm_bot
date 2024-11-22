a85926aff915af83a31fe26a6eaca3a17769ef36
Sanly Ozgerish <sh.alyyew2019@gmail.com>
2024-11-22 15:49:26 +0500

    compiler added

--- index.js
+++ index.js
@@ -1,45 +1,95 @@
-const Telegraf = require('telegraf')
-const express = require('express');
+const Telegraf = require("telegraf");
+const express = require("express");
+const OpenAI = require("openai");
+const axios = require("axios");
+const jsdom = require("jsdom");
+const cors = require("cors");
 
-const app = express()
-const port = 3000
-require('dotenv').config()
-const bot = new Telegraf.Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Salam bu kotlin.tm!'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Salam diymesen hic zat etjek dal!'));
-bot.hears('kotlin',(ctx)=> ctx.reply("Kotlin-da name soragyn bar?"));
-bot.command('help', async (ctx) => {
-  await ctx.sendMessage("Hello click this button", {
+const app = express();
+const port = 3000;
+require("dotenv").config();
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+const openAi = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+});
+const bot = new Telegraf.Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) =>
+  ctx.reply("Salam kotlin kod yazyp ugradyn we rezultady gorun!!!")
+);
+bot.command("help", async (ctx) => {
+  await ctx.sendMessage("Meselemin gornushini saylan!!!", {
     reply_markup: {
       keyboard: [
-        [{text: "Ktor"}],
-        [{text:"Kotlin russian documentation"}]
-      ]
-    }
-  })
+        [
+          {
+            text: `fun main() {
+                  println("Salam dunya");
+              }`,
+          },
+        ],
+        [
+          {
+            text: `fun main() {
+                for (i in 1..3) {
+                  println(i)
+              }
+            }`,
+          },
+        ],
+      ],
+    },
+  });
 });
-bot.on("message", async ctx => {
+bot.on("message", async (ctx) => {
   // bad, you did not await ctx.reply()
-  ctx.reply("Hello there!");
-  
-  // good, you awaited your requests
-  await ctx.reply("Hello there!");
-  
-  // also applies to any other async calls you may make
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-  await ctx.reply(JSON.stringify(data).substring(0, 4000));
-  
+  ctx.reply("Ustinde ishlenyar...");
+  ctx.reply("Kotlin kod yuklenyar...");
+  const res = await axios.post(
+    "https://api.kotlinlang.org/api/2.0.21/compiler/run",
+    {
+      args: "",
+      files: [
+        {
+          name: "File.kt",
+          text: ctx.message.text,
+          publicId: "",
+        },
+      ],
+      confType: "java",
+    }
+  );
+  try {
+    const message = res.data.text
+      .replace(
+        "<outStream>",
+        "```\n/*🎉 USTUNLIKLI ISHLEDI 🚀\n" + ctx.message.text + "*/\n"
+      )
+      .replace("</outStream>", "```");
+
+    await ctx.reply(message, { parse_mode: "MarkdownV2" });
+  } catch (err) {
+    await ctx.reply("🔴 Yalnyshlyk yuze cykdy ❌");
+    ctx.reply(res.data.errors["File.kt"].map((it) => it.message));
+  }
+  //var i = 0;while (i < 5) {println(i);i++;}
   // returning calls is also okay, because the promise will be returned
-  return res.statusText;
+  return "OK";
 });
 bot.launch();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", async (req, res) => {
+  const chatCompletion = await openAi.chat.completions.create({
+    messages: [{ role: "user", content: "write calculater jetpack compose" }],
+    model: "gpt-4o-mini",
+  });
+  console.log(chatCompletion);
+
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
